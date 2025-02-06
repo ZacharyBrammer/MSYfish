9c1135c6c19020b55c61a9204e20654a0ea42f1c
ZacharyBrammer <zacharyj.brammer@gmail.com>
2025-02-05 21:29:58 -0500

    Finish Catch Graphing

--- plotting.py
+++ plotting.py
@@ -4,19 +4,28 @@ import matplotlib.pyplot as plt
 import netCDF4 as nc
 import numpy as np
 
+
 def plot_simulation(
         path: str, # path to simulation output
     ):
     # Read the dataset
     biodata = nc.Dataset(path, "r")
 
-    # Subtract 100 from all years to remove stabilization period
+    # Get years. If simulation ended early, get index of last year
     years = biodata.variables["ftime"][:].data[100:]
+    endsEarly = np.where(years == 0)[0].size != 0
+    if endsEarly:
+        last = np.where(years == 0)[0][0]
+    else:
+        last = None
+
+    # Subtract 100 from all years to remove stabilization period
     years -= 100
+    years = years[:last]
 
     # Plot biomass per stock
     # Get per-stock biomass data
-    stockBiomass = biodata.variables["stock_biomass"][:].data[100:]
+    stockBiomass = biodata.variables["stock_biomass"][:].data[100:][:last]
 
     # Create Legend titles
     stocks = []
@@ -37,17 +46,21 @@ def plot_simulation(
 
     # Plot catch over time
     # Get catch data
-    catch = biodata.variables["catch"][:].data[100:]
+    catch = biodata.variables["catch"][:].data[100:][:last]
 
-    # Select just the caught # data
-    catch = catch[:, :, 0]
+    # Select just the caught weight data
+    catch = catch[:, :, 1]
 
     catchFig = plt.figure("Catch Over Time")
     plt.plot(years, catch)
+    plt.title("Catch Per Stock vs Time")
+    plt.xlabel("Time (years)")
+    plt.ylabel("Catch (kg)")
+    plt.ylim(bottom=0)
+    plt.gca().legend(stocks)
     
     plt.show()
 
     
     # Close file
     biodata.close()
-
