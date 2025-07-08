ef49c4c8aff8c14c67d4a0a4622857b3cc05ee49
ZacharyBrammer <zacharyj.brammer@gmail.com>
2025-07-08 15:44:47 -0400

    Analyze
    Good chunk of the analyze page in spanish

--- analyze.py
+++ analyze.py
@@ -3,35 +3,38 @@ import os
 import netCDF4 as nc
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
+import plotly.graph_objects as go # type: ignore
 import streamlit as st
 
 from plotting import plot_simulation
+from translate import Translator
 
 
 # Method to allow plotting/viewing of previous simulations
 def analyze(labels):
+    translator = Translator(st.session_state.language)
+    t = translator.translate
+
     # Select which simulation to plot
     base = f"simulations/{st.session_state.id}/"
     folders = [""] + sorted(os.listdir(base))
-    # TODO: Spanish
-    folder = st.selectbox(label="Folder", options=folders)
+    folder = st.selectbox(label=t("folder"), options=folders)
 
     # Select folder
     if folder != "":
         # Get all species in the folder. If there's multiple species, allow choice
         speciess = [""] + os.listdir(f"{base}/{folder}")
         if len(speciess) > 2:
-            # TODO: Spanish
-            species = st.selectbox(label="Species", options=sorted(
-                os.listdir(f"{base}/{folder}")))
+            species = st.selectbox(
+                label=t("species"),
+                options=sorted(os.listdir(f"{base}/{folder}"))
+            )
         else:
             species = speciess[-1]
 
         # Get all simulations in the folder and let user select
         simulations = [""] + os.listdir(f"{base}/{folder}/{species}")
-        # TODO: Spanish
-        simulation = st.selectbox(label="Simulation", options=simulations)
+        simulation = st.selectbox(label=t("simulation"), options=simulations)
     else:
         # If blank folder is selected set species and simulation to blank
         species = simulation = ""
@@ -55,9 +58,8 @@ def analyze(labels):
 
         # Download button for the simulation file
         with open(path, "rb") as file:
-            # TODO: Spanish
             st.download_button(
-                label="Download NetCDF file",
+                label=t("download"),
                 data=file,
                 file_name=f"{simulation}",
                 icon=":material/download:",
@@ -67,12 +69,10 @@ def analyze(labels):
         # TODO: allow user to make table from whatever variales they want, turn that into plots
         variable_desc = {}
         for variable in biodata.variables:
-            variable_desc[variable] = biodata.variables[variable].__dict__[
-                "long_name"]
+            variable_desc[variable] = biodata.variables[variable].__dict__["long_name"]
 
-        # TODO: Spanish
-        st.write("Create custom table/plot:")
-        st.write("Dataset Variables")
+        st.write(t("create_custom"))
+        st.write(t("dataset_vars"))
         st.json(variable_desc, expanded=0)
 
         # Remove variables that aren't time-related/would be difficult for users to make sense of
@@ -86,9 +86,10 @@ def analyze(labels):
         # Too many things for a user to reasonably be able to see on a plot
         selectable_vars.pop("fish")
 
-        # TODO: Spanish
         variables = st.multiselect(
-            label="Select Variables (Custom Plotting)", options=selectable_vars)
+            label=t("var_select"),
+            options=selectable_vars
+        )
 
         # TODO: for each selected: get title, get data, if more than 1d do a for loop to add all bins/stock with label
         # Collect data user selected, start with time variable
@@ -111,17 +112,21 @@ def analyze(labels):
                     match variable:
                         case "stock_size" | "stock_biomass" | "stock_recruit" | "resource":
                             var_name = variable.split("_")[-1]
-                            units = biodata.variables[variable].getncattr(
-                                "units")
-                            data = pd.DataFrame(data, columns=[
-                                                f"stock {i + 1} {var_name} ({units})" for i in range(data.shape[1])])
-                            selected_data = pd.concat(
-                                [selected_data, data], axis=1)
+                            units = biodata.variables[variable].getncattr("units")
+                            data = pd.DataFrame(
+                                data,
+                                columns=[f"{t("stock")} {i + 1} {t(var_name)} ({(units)})" for i in range(data.shape[1])]
+                            )
+                            selected_data = pd.concat([selected_data, data], axis=1)
                         case "reproduction" | "mortality":
                             data = pd.DataFrame(
-                                data, columns=[f"{variable} biomass (kg)", f"{variable} number (#)"])
-                            selected_data = pd.concat(
-                                [selected_data, data], axis=1)
+                                data,
+                                columns=[
+                                    f"{t(variable)} {t("biomass")} (kg)",
+                                    f"{t(variable)} {t("number")} (#)"
+                                ]
+                            )
+                            selected_data = pd.concat([selected_data, data], axis=1)
                 case 3:
                     # TODO: figure out this, gonna take too long so it's a later problem
                     # Some 3D variables have different units/sizes so handle differently
@@ -138,18 +143,24 @@ def analyze(labels):
         if selected_data.shape[1] > 0:
             figLayout = go.Layout(
                 title={
-                    "text": "Custom Plot", # TODO: Spanish
+                    "text": t("custom_plot"),
                     "x": 0.5,  # Center title on plot
                     "xanchor": "center",
                 },
                 # Set labels along with range
-                xaxis=dict(title="Time (years)", range=[0, None]), # TODO: Spanish
-                yaxis=dict(title="Selected Vars", range=[0, None]), # TODO: Spanish
+                xaxis=dict(
+                    title=t("years_x"),
+                    range=[0, None]
+                ),
+                yaxis=dict(
+                    title=t("cust_y"),
+                    range=[0, None]
+                ), 
                 template="plotly"  # Default dark theme
             )
             # If ends early, modify title using html to add warning
             if endsEarly:
-                figLayout.title["text"] = "Custom Plot <br><sup>Warning: population crashed during simulation</sup>" # TODO: Spanish
+                figLayout.title["text"] = f"{t("custom_plot")} <br><sup>{t("crash_warning")}</sup>" # type: ignore
 
             fig = go.Figure(layout=figLayout)
 
