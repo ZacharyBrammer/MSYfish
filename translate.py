6120a368885944b04a1c43baf6aad32034f62ca2
ZacharyBrammer <zacharyj.brammer@gmail.com>
2025-07-09 16:13:15 -0400

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,14 +1,15 @@
 import json
 
+
 class Translator:
     def __init__(self, lang="en", path="labels.json"):
         with open(path, "r", encoding="utf-8") as f:
             self.labels = json.load(f)
         self.lang = lang
-    
+
     def set_lang(self, lang):
         self.lang = lang
-    
+
     def translate(self, key, **kwargs):
         value = self.labels.get(key, {}).get(self.lang, key)
         return value.format(**kwargs)
